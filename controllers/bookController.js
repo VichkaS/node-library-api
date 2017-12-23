dfdbbe59567d2026d028bd6cf6c6c0b70bf85221
Takhir Nafikov <takhir.learn@gmail.com>
2017-12-23 16:31:25 +0300

    finished bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const Book = require('../models/Book');
+const User = require('../models/User');
 
 exports.addBook = async (req, res) => {
     try {
@@ -53,6 +54,84 @@ exports.getBookById = async (req, res) => {
     }
 };
 
-exports.getReadBooks = async (req, res) => {
-    
+exports.getReadsBooks = async (req, res) => {
+    try {
+        const user = await User.findOne({_id: req.userId});
+        res.json({
+            books: user.reads
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            error_code: 500,
+            message: err.message
+        })
+    }
+};
+
+exports.getFavoritesBooks = async (req, res) => {
+    try {
+        const user = await User.findOne({_id: req.userId});
+        res.json({
+            books: user.favorites
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({
+            error_code: 500,
+            message: err.message
+        })
+    }
+};
+
+exports.setReadBook = async (req, res) => {
+    try {
+        const user = await User.findOne({_id: req.userId});
+        const index = user.reads.indexOf(req.params.id);
+        if (index => 0) {
+            user.reads.splice(index, 1);
+            await user.save();
+            res.json({
+                read: false
+            });
+        } else {
+            user.reads.push(req.params.id);
+            await user.save();
+            res.json({
+                read: true
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.json({
+            error_code: 500,
+            message: err.message
+        })
+    }
+};
+
+exports.setFavoriteBook = async (req, res) => {
+    try {
+        const user = await User.findOne({_id: req.userId});
+        const index = user.favorites.indexOf(req.params.id);
+        if (index => 0) {
+            user.favorites.splice(index, 1);
+            await user.save();
+            res.json({
+                favorite: false
+            });
+        } else {
+            user.favorites.push(req.params.id);
+            await user.save();
+            res.json({
+                favorite: true
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.json({
+            error_code: 500,
+            message: err.message
+        })
+    }
 };
